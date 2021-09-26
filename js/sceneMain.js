a48ab71d181e97d11d1a79116571e20ff6d5a273
Daniel <itzdanielmichael@gmail.com>
2021-09-25 23:56:21 -0400

    update sceneMAin.js create fontLoader.js

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -8,10 +8,10 @@ class SceneMain extends Phaser.Scene {
   preload() {
     // load our images or sounds
     // this.load.image('face', 'images/face.png'); // this.load.image('key', 'path')
-    this.load.spritesheet('boy', 'images/boy.png', {
-      frameWidth: 120, // width of each frame
-      frameHeight: 200, // height of each frame, these have to be accurate...
-    });
+    //   this.load.spritesheet('boy', 'images/boy.png', {
+    //     frameWidth: 120, // width of each frame
+    //     frameHeight: 200, // height of each frame, these have to be accurate...
+    //   });
   }
 
   create() {
@@ -31,56 +31,65 @@ class SceneMain extends Phaser.Scene {
     // this.face.displayHeight = 50 // set width to 100 pixels
     // this.face.scaleY = this.face.scaleX // proportional height
 
-    this.char = this.add.sprite(0, Y_AXIS_CENTER, 'boy'); // x, y, key
+    // this.char = this.add.sprite(0, Y_AXIS_CENTER, 'boy'); // x, y, key
 
-    let frameNames = this.anims.generateFrameNumbers('boy'); // get amount of frames for sprite (needs key)
+    // let frameNames = this.anims.generateFrameNumbers('boy'); // get amount of frames for sprite (needs key)
 
     // creating an animation
-    this.anims.create({
-      key: 'walk',
-      frames: frameNames,
-      // frames: [
-      // key of sprite, frame number going from 0 to 5, x image has x many frames
-      //{ key: 'boy', frame: frameNumber (0,1,2, etc...) },
-      // { key: 'boy', frame: 0 },
-      // { key: 'boy', frame: 1 },
-      // { key: 'boy', frame: 2 },
-      // { key: 'boy', frame: 3 },
-      // { key: 'boy', frame: 4 },
-      // { key: 'boy', frame: 5 },
-      // ],
-      frameRate: 8, // speed of animation
-      repeat: -1, // repeat -1 is infinite, enter other positive number for set number of animation loops
-    });
+    // this.anims.create({
+    //   key: 'walk',
+    //   frames: frameNames,
+    //   // frames: [
+    //   // key of sprite, frame number going from 0 to 5, x image has x many frames
+    //   //{ key: 'boy', frame: frameNumber (0,1,2, etc...) },
+    //   // { key: 'boy', frame: 0 },
+    //   // { key: 'boy', frame: 1 },
+    //   // { key: 'boy', frame: 2 },
+    //   // { key: 'boy', frame: 3 },
+    //   // { key: 'boy', frame: 4 },
+    //   // { key: 'boy', frame: 5 },
+    //   // ],
+    //   frameRate: 8, // speed of animation
+    //   repeat: -1, // repeat -1 is infinite, enter other positive number for set number of animation loops
+    // });
 
-    this.char.play('walk'); // play animation('key')
+    // this.char.play('walk'); // play animation('key')
 
-    this.doWalk();
+    // this.doWalk();
+
+    // adding text
+    // this.text1 = this.add.text(X_AXIS_CENTER, Y_AXIS_CENTER, 'hello', {
+    //   // jss
+    //   fontFamily: 'Anton',
+    //   color: '#ff0000',
+    //   fontSize: '40px',
+    // });
+    // this.text1.setOrigin(0.5, 0.5);
   }
-  doWalk() {
-    // another way of moving sprite
-    this.tweens.add({
-      targets: this.char,
-      duration: 5000, // 1 second in ms
-      x: game.config.width, // go in x axis
-      y: 0, // go in y axis
-      alpha: 0, // transparency
-      onComplete: this.onCompleteHandler.bind(this), // function that runs when the tween sequence end, put scope of scene to the function with .bind
-      onCompleteParams: [this], // pas it to scope of onCompleteHandler
-    });
-  }
+  // doWalk() {
+  //   // another way of moving sprite
+  //   this.tweens.add({
+  //     targets: this.char,
+  //     duration: 5000, // 1 second in ms
+  //     x: game.config.width, // go in x axis
+  //     y: 0, // go in y axis
+  //     alpha: 0, // transparency
+  //     onComplete: this.onCompleteHandler.bind(this), // function that runs when the tween sequence end, put scope of scene to the function with .bind
+  //     onCompleteParams: [this], // pas it to scope of onCompleteHandler
+  //   });
+  // }
 
-  onCompleteHandler(tween, targets, custom) {
-    let Y_AXIS_CENTER = game.config.height / 2;
+  // onCompleteHandler(tween, targets, custom) {
+  //   let Y_AXIS_CENTER = game.config.height / 2;
 
-    console.log('complete!');
-    let char = targets[0]; // have to access it as a local variable through targets for the next lines to work, can't use this.char
-    char.x = 0;
-    char.y = Y_AXIS_CENTER;
-    char.alpha = 1;
+  //   console.log('complete!');
+  //   let char = targets[0]; // have to access it as a local variable through targets for the next lines to work, can't use this.char
+  //   char.x = 0;
+  //   char.y = Y_AXIS_CENTER;
+  //   char.alpha = 1;
 
-    this.doWalk();
-  }
+  //   this.doWalk();
+  // }
   update() {
     // constant running loop, can remember it by comparing to a useEffect with no dependency array.
     // WAY 1 OF MOVING SPRITES
